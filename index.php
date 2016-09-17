ce192229bec38069d12b3f7c32361c9bfd265072
DM <session2@START-Coding-MBP-16.local>
2016-09-16 17:55:26 -0700

    This project includes: - INCLUDE function -  ATTRIBUTES - METHOD -  GET_ & SET_ - CONSTRUCT OBJECT - CONSTRUCTOR METHOD

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<!-- INCLUDE FUNCTION-->
+<?php
+include 'second.php';
+?>
+
+<?php
+
+class fruitSalad{
+    var $name;
+// FOUR ATTRIBUTES
+    public $fruit1;
+    public $fruit2;
+    public $fruit3;
+    public $ingredient1;
+
+/* METHOD 1 */
+    private function cook($fruit1, $fruit2, $ingredient1)
+    {
+        echo "we're making an" . $fruit1 . $fruit2 . ' and ' . $ingredient1 . "salad!";
+    }
+/* METHOD 2 */
+    private function making(){
+        echo "we're making a fruit salad!";
+    }
+//    GET_ //
+    function get_name(){
+    return $this->name;
+}
+//    SET_ //
+
+    function __construct($fruit3){
+        $this-> set_name("pineapples");
+    }
+}
+
+
+
+ new fruitSalad('apple','pear','marshmallow');
+
+?>
---
+++ second.php
@@ -0,0 +1,15 @@
+
+<!-- INCLUDE FUNCTION -->
+<?php
+include 'index.php';
+?>
+
+<?php
+
+
+// CONSTRUCT OBJECT + CONSTRUCTOR METHOD
+function __construct($fruit_name){
+    $this-> name= $fruit_name;
+}
+
+?>
